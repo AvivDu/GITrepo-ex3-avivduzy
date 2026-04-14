59988abeaeea6469d30b7cdc870d61f9fab3fd08
AvivDu <avivduzy22@gmail.com>
2026-04-14 15:26:46 +0300

    4 commit

--- avivduzy.java
+++ avivduzy.java
@@ -1,7 +1,7 @@
 public class avivduzy{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("avivduzy NEW 3..."+i);
+            System.out.println("avivduzy NEW 4..."+i);
         }
     }
 }
