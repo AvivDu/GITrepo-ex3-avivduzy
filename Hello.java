25e17e93415fe10989eda8372347ad2a1abc7953
AvivDu <avivduzy22@gmail.com>
2026-04-14 15:37:14 +0300

    7 commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Hello World NEW 6..."+i);
+            System.out.println("Hello World NEW 7..."+i);
         }
     }
 }
